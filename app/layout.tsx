fa4d1fbe378482790d2b2e9ba5a60899ceb7df4b
ahmedbarakat98 <ahmedbarakat9850@gmail.com>
2026-05-08 23:51:07 +0300

    Yarab

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} bg-black ${playfair.variable} ${rubik.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
         <ClientLayout>
           <Navbar />
           {children}
